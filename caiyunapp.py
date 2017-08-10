c960cdbb400eeeadfd62fc2b189f416c8fbde7dc
L <lzps@users.noreply.github.com>
2017-08-10 19:37:33 +0800

    style: 优化格式

--- caiyunapp.py
+++ caiyunapp.py
@@ -24,7 +24,7 @@ class caiyunapp(object):
     """API说明：http://wiki.swarma.net/index.php/彩云天气API/v2"""
 
     def __init__(self, key, location):
-        """key即https://www.caiyunapp.com/dev_center/login.html 登录后的API密钥
+        """key 即 https://www.caiyunapp.com/dev_center/login.html 登录后的API密钥
         location即经纬度，注意纬度在前经度在后 e.g.:"39.93,116.40\""""
         self.key = key
         self.loc = ",".join(location.split(",")[::-1])
@@ -37,8 +37,10 @@ class caiyunapp(object):
         """以字典形式返回实时天气状况"""
         t = str(self.dnow['result'])
         t = sub(r"'status.*?,", '', t)
-        a = ['cloudrate', 'humidity', 'precipitation', 'skycon', 'wind', 'aqi', 'temperature', 'local', 'datasource', 'nearest', 'intensity', 'direction', 'speed', 'distance']
-        b = ['云量', '相对湿度', '降水', '天气概况', '风', '空气质量指数', '温度', '本地的', '数据源', '最近的降水带', '强度', '风向', '风速', '距离']
+        a = ['cloudrate', 'humidity', 'precipitation', 'skycon', 'wind', 'aqi', 'temperature',
+             'local', 'datasource', 'nearest', 'intensity', 'direction', 'speed', 'distance']
+        b = ['云量', '相对湿度', '降水', '天气概况', '风', '空气质量指数', '温度',
+             '本地的', '数据源', '最近的降水带', '强度', '风向', '风速', '距离']
         for i in range(0, len(a)):
             t = t.replace(a[i], b[i])
         infon = eval(t)
@@ -50,8 +52,10 @@ class caiyunapp(object):
         """以字典形式返回天级别的预报状况"""
         t = str(self.data['result']['daily'])
         t = sub(r"'status.*?,", '', t)
-        a = ['avg', 'date', 'datetime', 'time', 'max', 'min', 'astro', 'sunrise', 'sunset', 'desc', 'index', 'carWashing', 'cloudrate', 'coldRisk', 'dressing', 'humidity', 'precipitation', 'temperature', 'ultraviolet', 'wind', 'direction', 'speed']
-        b = ['平均', '日期', '日期', '时间', '最高', '最低', '日出日落', '日出时间', '日落时间', '介绍', '指数', '洗车指数', '云量', '感冒风险', '着装指数', '相对湿度', '降雨强度', '温度', '紫外线', '风况', '风向', '风力']
+        a = ['avg', 'date', 'datetime', 'time', 'max', 'min', 'astro', 'sunrise', 'sunset', 'desc', 'index', 'carWashing',
+             'cloudrate', 'coldRisk', 'dressing', 'humidity', 'precipitation', 'temperature', 'ultraviolet', 'wind', 'direction', 'speed']
+        b = ['平均', '日期', '日期', '时间', '最高', '最低', '日出日落', '日出时间', '日落时间', '介绍', '指数',
+             '洗车指数', '云量', '感冒风险', '着装指数', '相对湿度', '降雨强度', '温度', '紫外线', '风况', '风向', '风力']
         for i in range(0, len(a)):
             t = t.replace(a[i], b[i])
         infon = eval(t)
@@ -60,7 +64,8 @@ class caiyunapp(object):
 
 if __name__ == '__main__':
     from pprint import pprint
-    t = caiyunapp(input('请输入key：'), input('请输入经纬度 例如：location=39.93,116.40 （注意纬度在前经度在后）'))
+    t = caiyunapp(input('请输入key：'), input(
+        '请输入经纬度 例如：location=39.93,116.40 （注意纬度在前经度在后）'))
     print(t.dnow)
     print(t.data)
     while True:
--- thinkpage.py
+++ thinkpage.py
@@ -53,19 +53,23 @@ class thinkpage(object):
     """API说明：http://www.thinkpage.cn/doc/v2"""
 
     def __init__(self, key, location):
-        """key即http://www.thinkpage.cn/account 中账号资料的API密钥
+        """key 即 http://www.thinkpage.cn/account 中账号资料的API密钥
         location即经纬度，注意纬度在前经度在后 e.g.:"39.93,116.40\""""
         self.key = key
         self.loc = location.replace(',', ':')
-        self.dnow = requests.get('https://api.thinkpage.cn/v2/weather/now.json?key={0}&city={1}'.format(self.key, self.loc)).json()
-        self.data = requests.get('https://api.thinkpage.cn/v2/weather/future.json?key={0}&city={1}'.format(self.key, self.loc)).json()
+        self.dnow = requests.get(
+            'https://api.thinkpage.cn/v2/weather/now.json?key={0}&city={1}'.format(self.key, self.loc)).json()
+        self.data = requests.get(
+            'https://api.thinkpage.cn/v2/weather/future.json?key={0}&city={1}'.format(self.key, self.loc)).json()
         # 可合并请求 all.json
 
     def now(self):
         """以字典形式返回实时天气状况"""
         t = str(self.dnow['weather'][0])
-        a = ['last_update', 'temperature', 'city_name', 'now', 'air_quality', 'feels_like', 'humidity', 'visibility', 'wind_direction', 'wind_scale', 'wind_speed', 'wind_direction_degree', 'pressure', '_rising', 'text']
-        b = ['数据更新时间（该城市的本地时间）', '摄氏温度', '城市名', '实况', '空气质量', '体感温度', '相对湿度', '能见度', '风向', '风力等级', '风速', '风向角度', '气压', '升高', '天气现象']
+        a = ['last_update', 'temperature', 'city_name', 'now', 'air_quality', 'feels_like', 'humidity', 'visibility',
+             'wind_direction', 'wind_scale', 'wind_speed', 'wind_direction_degree', 'pressure', '_rising', 'text']
+        b = ['数据更新时间（该城市的本地时间）', '摄氏温度', '城市名', '实况', '空气质量', '体感温度',
+             '相对湿度', '能见度', '风向', '风力等级', '风速', '风向角度', '气压', '升高', '天气现象']
         for i in range(0, len(a)):
             t = t.replace(a[i], b[i])
         infon = eval(t)
@@ -77,17 +81,21 @@ class thinkpage(object):
         t = str(self.data['weather'][0])
         t = sub(r"'code1.*?,", '', t)
         t = sub(r"'code2.*?,", '', t)
-        a = ['last_update', 'city_name', 'future', 'date', 'text', 'high', 'low', 'wind', 'cop']
-        b = ['数据更新时间（该城市的本地时间）', '城市名', '预报', '日期', '现象', '最高温度', '最低温度', '风况', '降水概率']
+        a = ['last_update', 'city_name', 'future',
+             'date', 'text', 'high', 'low', 'wind', 'cop']
+        b = ['数据更新时间（该城市的本地时间）', '城市名', '预报', '日期',
+             '现象', '最高温度', '最低温度', '风况', '降水概率']
         for i in range(0, len(a)):
             t = t.replace(a[i], b[i])
         infon = eval(t)
         del(infon['city_id'])
         return infon
 
+
 if __name__ == '__main__':
     from pprint import pprint
-    t = thinkpage(input('请输入key：'), input('请输入经纬度 例如：location=39.93,116.40 （注意纬度在前经度在后）'))
+    t = thinkpage(input('请输入key：'), input(
+        '请输入经纬度 例如：location=39.93,116.40 （注意纬度在前经度在后）'))
     print(t.dnow)
     print(t.data)
     while True:
